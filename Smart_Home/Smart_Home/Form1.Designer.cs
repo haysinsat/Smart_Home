dbec9f6fc0418e82542bf695539f7678125fbf8c
haysin <haysi@DESKTOP-GG0I92V>
2018-01-08 17:54:04 +0200

    Intial comit;

---
+++ Smart_Home/Smart_Home/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Smart_Home
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox9 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox10 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox11 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox12 = new System.Windows.Forms.RichTextBox();
+            this.general = new System.Windows.Forms.Button();
+            this.history = new System.Windows.Forms.Button();
+            this.setup = new System.Windows.Forms.Button();
+            this.richTextBox13 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox14 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox15 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(142, 20);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 93);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(142, 20);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox3.Location = new System.Drawing.Point(12, 119);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(142, 20);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox4.Location = new System.Drawing.Point(12, 145);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(142, 20);
+            this.richTextBox4.TabIndex = 3;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox5.Location = new System.Drawing.Point(159, 67);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(46, 20);
+            this.richTextBox5.TabIndex = 4;
+            this.richTextBox5.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox6.Location = new System.Drawing.Point(159, 93);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(46, 20);
+            this.richTextBox6.TabIndex = 5;
+            this.richTextBox6.Text = "";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox7.Location = new System.Drawing.Point(159, 119);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(46, 20);
+            this.richTextBox7.TabIndex = 6;
+            this.richTextBox7.Text = "";
+            // 
+            // richTextBox8
+            // 
+            this.richTextBox8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox8.Location = new System.Drawing.Point(159, 145);
+            this.richTextBox8.Name = "richTextBox8";
+            this.richTextBox8.Size = new System.Drawing.Size(46, 20);
+            this.richTextBox8.TabIndex = 7;
+            this.richTextBox8.Text = "";
+            // 
+            // richTextBox9
+            // 
+            this.richTextBox9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox9.Location = new System.Drawing.Point(211, 67);
+            this.richTextBox9.Name = "richTextBox9";
+            this.richTextBox9.Size = new System.Drawing.Size(46, 20);
+            this.richTextBox9.TabIndex = 8;
+            this.richTextBox9.Text = "";
+            // 
+            // richTextBox10
+            // 
+            this.richTextBox10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox10.Location = new System.Drawing.Point(210, 93);
+            this.richTextBox10.Name = "richTextBox10";
+            this.richTextBox10.Size = new System.Drawing.Size(46, 20);
+            this.richTextBox10.TabIndex = 9;
+            this.richTextBox10.Text = "";
+            // 
+            // richTextBox11
+            // 
+            this.richTextBox11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox11.Location = new System.Drawing.Point(210, 119);
+            this.richTextBox11.Name = "richTextBox11";
+            this.richTextBox11.Size = new System.Drawing.Size(46, 20);
+            this.richTextBox11.TabIndex = 10;
+            this.richTextBox11.Text = "";
+            // 
+            // richTextBox12
+            // 
+            this.richTextBox12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox12.Location = new System.Drawing.Point(210, 145);
+            this.richTextBox12.Name = "richTextBox12";
+            this.richTextBox12.Size = new System.Drawing.Size(46, 20);
+            this.richTextBox12.TabIndex = 11;
+            this.richTextBox12.Text = "";
+            // 
+            // general
+            // 
+            this.general.Location = new System.Drawing.Point(12, 484);
+            this.general.Name = "general";
+            this.general.Size = new System.Drawing.Size(100, 23);
+            this.general.TabIndex = 12;
+            this.general.Text = "Головна";
+            this.general.UseVisualStyleBackColor = true;
+            // 
+            // history
+            // 
+            this.history.Location = new System.Drawing.Point(118, 484);
+            this.history.Name = "history";
+            this.history.Size = new System.Drawing.Size(75, 23);
+            this.history.TabIndex = 13;
+            this.history.Text = "Історія";
+            this.history.UseVisualStyleBackColor = true;
+            // 
+            // setup
+            // 
+            this.setup.Location = new System.Drawing.Point(199, 484);
+            this.setup.Name = "setup";
+            this.setup.Size = new System.Drawing.Size(98, 23);
+            this.setup.TabIndex = 14;
+            this.setup.Text = "Налаштування";
+            this.setup.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox13
+            // 
+            this.richTextBox13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox13.Location = new System.Drawing.Point(12, 171);
+            this.richTextBox13.Name = "richTextBox13";
+            this.richTextBox13.Size = new System.Drawing.Size(142, 20);
+            this.richTextBox13.TabIndex = 15;
+            this.richTextBox13.Text = "";
+            // 
+            // richTextBox14
+            // 
+            this.richTextBox14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox14.Location = new System.Drawing.Point(211, 171);
+            this.richTextBox14.Name = "richTextBox14";
+            this.richTextBox14.Size = new System.Drawing.Size(46, 20);
+            this.richTextBox14.TabIndex = 17;
+            this.richTextBox14.Text = "";
+            // 
+            // richTextBox15
+            // 
+            this.richTextBox15.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox15.Location = new System.Drawing.Point(159, 171);
+            this.richTextBox15.Name = "richTextBox15";
+            this.richTextBox15.Size = new System.Drawing.Size(46, 20);
+            this.richTextBox15.TabIndex = 16;
+            this.richTextBox15.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(932, 519);
+            this.Controls.Add(this.richTextBox14);
+            this.Controls.Add(this.richTextBox15);
+            this.Controls.Add(this.richTextBox13);
+            this.Controls.Add(this.setup);
+            this.Controls.Add(this.history);
+            this.Controls.Add(this.general);
+            this.Controls.Add(this.richTextBox12);
+            this.Controls.Add(this.richTextBox11);
+            this.Controls.Add(this.richTextBox10);
+            this.Controls.Add(this.richTextBox9);
+            this.Controls.Add(this.richTextBox8);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.RichTextBox richTextBox8;
+        private System.Windows.Forms.RichTextBox richTextBox9;
+        private System.Windows.Forms.RichTextBox richTextBox10;
+        private System.Windows.Forms.RichTextBox richTextBox11;
+        private System.Windows.Forms.RichTextBox richTextBox12;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button general;
+        private System.Windows.Forms.Button history;
+        private System.Windows.Forms.Button setup;
+        private System.Windows.Forms.RichTextBox richTextBox13;
+        private System.Windows.Forms.RichTextBox richTextBox14;
+        private System.Windows.Forms.RichTextBox richTextBox15;
+    }
+}
+
---
+++ Smart_Home/Smart_Home/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace Smart_Home
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            timer1.Start();
+
+            richTextBox1.Text = "Температура Бойлер";  richTextBox9.Text = "  C";
+            richTextBox2.Text = "Вода на бак (Л/Год)"; richTextBox10.Text = "Л/Г";
+            richTextBox3.Text = "Вода на бак (Л/Доба)"; richTextBox11.Text = "Л/Д";
+            richTextBox4.Text = "Вода на бак (Л)";  richTextBox12.Text = "  Л";
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            HttpClient clientPS = new HttpClient();
+            string responseStringPS = await clientPS.GetStringAsync("http://192.168.1.41/dataPS");
+
+            dynamic jsonDataPS = JObject.Parse(responseStringPS);
+
+            string ipPS = jsonDataPS.ip;
+            string tempBoyler = jsonDataPS.tempBoyler;
+            string l_hour = jsonDataPS.l_hour;
+            string flow_val = jsonDataPS.flow_val;
+            string flow_day = jsonDataPS.flow_day;
+            string tempBoylerCF = jsonDataPS.tempBoylerCF;
+            string tempBoylerCF1 = jsonDataPS.tempBoylerCF1;
+            //string PSarray = jsonDataPS.PSarray[58];
+            
+            JArray PSarray = JArray.Parse(jsonDataPS["PSarray"].ToString());
+            var PSarrayData = PSarray.ToObject<List<int>>();
+
+            richTextBox5.Text = tempBoyler;
+            richTextBox6.Text = l_hour;
+            richTextBox7.Text = flow_day;
+            richTextBox8.Text = flow_val;
+            //richTextBox13.Text = PSarray;
+            richTextBox14.Text = tempBoylerCF1;
+            richTextBox15.Text = tempBoylerCF;
+        }
+
+        
+    }
+}
